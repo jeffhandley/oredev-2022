20a3248bccf78eff253674b35be3bf2fa3ecb49e
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-10-19 18:50:51 -0700

    Add beginning of the SessionMatch demo

---
+++ SessionMatch/ArgumentNullException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionMatch
+{
+    internal static class ArgumentNullException
+    {
+        public static void ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            System.ArgumentNullException.ThrowIfNullOrEmpty(argument, paramName);
+        }
+
+        public static void ThrowIfNullOrEmpty([NotNull] string?[]? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            System.ArgumentNullException.ThrowIfNull(argument, paramName);
+            if (argument.Length == 0) { throw new System.ArgumentNullException(paramName); }
+        }
+    }
+}
---
+++ SessionMatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SessionMatch
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(args);
+            ArgumentNullException.ThrowIfNullOrEmpty(args[0]);
+
+            var regex = args[0].ToLowerInvariant() switch
+            {
+                ".net" or "dotnet" => DotNet(),
+                _ => throw new ArgumentException("Unrecognized tag", args[0])
+            };
+
+            bool hasMatch = false;
+            foreach (string session in Directory.EnumerateFiles("Sessions", "*.txt"))
+            {
+                var text = File.ReadAllText(session);
+
+                if (regex.IsMatch(text))
+                {
+                    hasMatch = true;
+                    Console.WriteLine(session[9..^4]);
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("No matching sessions");
+            }
+        }
+
+        [GeneratedRegex("(.net|dotnet)", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled, "en-US")]
+        private static partial Regex DotNet();
+    }
+}
