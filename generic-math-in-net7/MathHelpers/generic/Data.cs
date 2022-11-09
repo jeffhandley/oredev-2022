2c0c0484dc78ad60641def49948c686e669e1bf0
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-11-08 20:46:50 -0800

    Reset to start of generic-math demo

--- generic-math-in-net7/MathHelpers/generic/Data.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MathHelpers;
-
-internal static partial class Program
-{
-    public static readonly Int128[] int128s = new Int128[] { Int128.MinValue, Int128.Zero, Int128.MaxValue };
-    public static readonly Half[] halves = new Half[] { Half.MinValue, Half.Zero, Half.MaxValue };
-}
